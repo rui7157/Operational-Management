8ee4bd06f66601ff0e6d27b33fd50d83c8dc01f5
bycidao <18161327091@163.com>
2016-03-15 11:51:18 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -14,9 +14,17 @@ class TestDev(object):
     DB='cdms'
     CHARSET='utf8'
 
+class CidaoDev(object):
+    SECRET_KEY = "c61f1f9e762e16831f676528b89b31d"
+    HOST='127.0.0.1'
+    USER='root'
+    DB='cdms'
+    CHARSET='utf8'
+
 
 
 config = {
     'MainConfig': MainConfig,
-    'TestDec':TestDev
+    'TestDec':TestDev,
+    'CidaoDev':CidaoDev
 }
